9c0cac2e47245bf71db5636cc27165fbc06ebaf5
quinnss <quinnss007@gmail.com>
2018-12-18 19:00:26 -0500

    we added a badguy and a coin that collides with the player

--- platformer/game.js
+++ platformer/game.js
@@ -1,25 +1,57 @@
 
 const keys = 'LEFT,RIGHT,UP,DOWN,SPACE,W,A,S,D'
-let pl, plats, k
+let pl, plats, k, bad, coins
 
 function preload() {
   this.load.image("bg", "./assets/star-bg.png")
   this.load.image("pl", "./assets/ship.png")
   this.load.image("plat", "./assets/platform.png")
+  this.load.image("bad", "./assets/doge.png")
+  this.load.image("coin", "./assets/dcoin.png")
 }
 
 function create() {
   this.add.image(0, 0, "bg").setOrigin(0, 0)
   pl = this.physics.add.sprite(100, 100, "pl").setOrigin(0, 0)
   pl.setCollideWorldBounds(true)
-  pl.setBounce(0)
+  pl.setVelocity(400)
+  pl.setScale(1)
+  pl.setGravityY(1200)
+
+  bad = this.physics.add.sprite(100, 100, "bad").setOrigin(0, 0)
+  bad.setCollideWorldBounds(true)
+  bad.setScale(0.2)
+  bad.setVelocity(300)
+  bad.setBounce(1)
 
   plats = this.physics.add.staticGroup()
   plats.create(400, 300, 'plat')
   plats.create(150, 250, 'plat').setScale(1.5, 1).refreshBody()
   plats.create(350, 100, 'plat')
 
+
+  let c
+  coins = this.physics.add.group()
+  c = coins.create(400,20, 'coin')
+  c.setScale(0.1)
+  c.setCollideWorldBounds(true)
+  c.setGravityY(1200)
+
+  c = coins.create(450,20, 'coin')
+  c.setScale(0.1)
+  c.setCollideWorldBounds(true)
+  c.setGravityY(1200)
+
+  c = coins.create(200,20, 'coin')
+  c.setScale(0.1)
+  c.setCollideWorldBounds(true)
+  c.setGravityY(1200)
+
   this.physics.add.collider(pl, plats)
+  this.physics.add.collider(bad, plats)
+  this.physics.add.collider(pl, bad)
+  this.physics.add.collider(pl, coins)
+  this.physics.add.collider(coins, plats)
 
   k = this.input.keyboard.addKeys(keys)
 }
@@ -36,9 +68,9 @@ function update() {
   if (pl.body.onFloor()) {
     if (k.UP.isDown) {
       pl.setVelocityY(-700)
+    }
+    pl.setDragX(2000)
   }
-  pl.setDragX(2000)
-}
 
 
 }
@@ -50,8 +82,6 @@ let config = {
   physics: {
     default: "arcade",
     arcade: {
-      gravity: { y: 1600 },
-      debug: false,
     },
   },
 }
